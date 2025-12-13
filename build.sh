915bc6e7cf3dd42dc14b817a441f8aabcb2f0ba5
CheBhoganadhuni <bhoganadhunichetan@gmail.com>
2025-12-13 17:04:35 +0530

    Fix Build

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Build Tailwind CSS (requires tailwind python package)
-python manage.py tailwind install
-python manage.py tailwind build
+# Build Tailwind CSS (Skipped - using pre-built CSS)
+# python manage.py tailwind install
+# python manage.py tailwind build
 
 # Collect static files
 python manage.py collectstatic --no-input
